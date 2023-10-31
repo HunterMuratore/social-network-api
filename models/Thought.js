1f7a7d4db36de36260392dce754dabde5b34460c
Hunter <muratoreh@gmail.com>
2023-10-30 22:03:57 -0400

    Create User and Thought models and submodel Reaction

---
+++ models/Thought.js
@@ -0,0 +1,60 @@
+const { Schema, model } = require('mongoose');
+
+const reactionSchema = new Schema({
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: new ObjectId()
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        tpye: Date,
+        default: Date.now,
+        get: function*(createdAt) {
+            return createdAt.toLocaleDateString('en-US'); // Formats string to MM/DD/YYYY
+        }
+    }
+})
+
+const thoughtSchema = new Schema({
+    thoughtText: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 280 
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: function(createdAt) {
+            return createdAt.toLocaleDateString('en-US');
+        }
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    reactions: [reactionSchema]
+}, {
+    virtuals: {
+        reactionCount: {
+            get() {
+                return this.reactions.length;
+            }
+        }
+    },
+    toJSON: {
+        virtuals: true
+    }
+});
+
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
---
+++ models/User.js
@@ -0,0 +1,50 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        validate: {
+            validator(val) {
+                return /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/gi.test(val);
+            },
+            message() {
+                return 'You must enter a valid email address'
+            }
+        }
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+        }
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ]
+}, {
+    virtuals: {
+        friendCount: {
+            get() {
+                return this.friends.length;
+            }
+        }
+    },
+    toJSON: {
+        virtuals: true
+    }
+});
+
+const User = model('User', userSchema);
+
+module.exports = User;
